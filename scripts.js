276fdcee63040b534bbafbf4562e0fe90c34ee45
Ana Carla Rodrigues <107411483+Ana40@users.noreply.github.com>
2022-09-17 16:12:42 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,8 @@
+# imersao.devPortfolioAlura
+Esse projeto foi onde aprendi a fazer um portifolio
+=========================================================================================================================
+
+
+function mudaModo(){
+  document.body.classList.toggle('dark')
+}
